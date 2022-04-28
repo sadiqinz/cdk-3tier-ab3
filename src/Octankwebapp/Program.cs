f9213d2afa2a75efc9e39b6ef1cdc2b46d9cb34a
mrsadiq <mrsadiq@amazon.com>
2022-04-29 06:47:49 +1200

    Added Env in Pipeline

--- src/Octankwebapp/Program.cs
+++ src/Octankwebapp/Program.cs
@@ -28,13 +28,13 @@ namespace Octankwebapp
 
                 // Uncomment the next block if you know exactly what Account and Region you
                 // want to deploy the stack to.
-                /*
+                
                 Env = new Amazon.CDK.Environment
                 {
-                    Account = "123456789012",
-                    Region = "us-east-1",
+                    Account = "808754908315",
+                    Region = "ap-southeast-2",
                 }
-                */
+                
 
                 // For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
             });
