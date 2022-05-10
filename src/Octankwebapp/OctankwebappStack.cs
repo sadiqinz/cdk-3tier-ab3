21dd7c6e4bab0ed391f12575135cd63dfb07e6d6
mrsadiq <mrsadiq@amazon.com>
2022-05-10 18:21:07 +1200

    Updated CloudFront Allowed Methods

--- src/Octankwebapp/OctankwebappStack.cs
+++ src/Octankwebapp/OctankwebappStack.cs
@@ -105,7 +105,7 @@ namespace Octankwebapp
             
             //Create a CloudFront Distribution and point to ALB
             new Distribution(this, "octankdistribution", new DistributionProps{
-                DefaultBehavior = new BehaviorOptions { Origin = new LoadBalancerV2Origin(lb), OriginRequestPolicy = OriginRequestPolicy.ALL_VIEWER, ViewerProtocolPolicy = ViewerProtocolPolicy.REDIRECT_TO_HTTPS },
+                DefaultBehavior = new BehaviorOptions { Origin = new LoadBalancerV2Origin(lb), OriginRequestPolicy = OriginRequestPolicy.ALL_VIEWER, ViewerProtocolPolicy = ViewerProtocolPolicy.REDIRECT_TO_HTTPS, AllowedMethods = AllowedMethods.ALLOW_ALL },
                 DomainNames = new [] { "www.octankfootwear.cloud" },
                 Certificate = acmCert
 
